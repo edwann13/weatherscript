0c312421a3a52c570f01b41456b81a9971658c43
Edwan Hernandez <edwanhernandez@gmail.com>
2014-12-22 16:31:54 -0800

    Now I am able to ask for a zip code and i am able to return the condition of that location.

--- weatherDemo.py
+++ weatherDemo.py
@@ -2,11 +2,13 @@ import  pywapi
 import string
 
 weather_com_result = pywapi.get_weather_from_weather_com('10001')
-yahoo_result = pywapi.get_weather_from_yahoo('10001')
+
+filename = raw_input('Please enter a zipcode: ')
+yahoo_result = pywapi.get_weather_from_yahoo(filename)
 noaa_result = pywapi.get_weather_from_noaa('KJFK')
 
-print "Weather.com says: It is " + string.lower(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in New York.\n\n"
+# print "Weather.com says: It is " + string.lower(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in New York.\n\n"
 
 print "Yahoo says: It is " + string.lower(yahoo_result['condition']['text']) + " and " + yahoo_result['condition']['temp'] + "C now in New York.\n\n"
-
-print "NOAA says: It is " + string.lower(noaa_result['weather']) + " and " + noaa_result['temp_c'] + "C now in New York.\n"
+print "this is the city : " + string.lower(yahoo_result['location']['city'])
+# print "NOAA says: It is " + string.lower(noaa_result['weather']) + " and " + noaa_result['temp_c'] + "C now in New York.\n"
